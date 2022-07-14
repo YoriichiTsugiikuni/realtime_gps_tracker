5b385fd092449e754dcbae9f9e9262be5907f12c
Romaine Dixon <romainedixon66@gmail.com>
2022-07-14 00:53:15 -0500

    database daddiii

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-#from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
 from marshmallow import Schema, ValidationError,fields
-#from bson.json_util import dumps
+from bson.json_util import dumps
 from json import loads
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+
+
 
 
 
@@ -18,8 +20,8 @@ info = {}
 app = Flask(__name__)
 CORS(app)
 #setup location of database
-#app.config["MONGO_URI"] = os.getenv("MONGO_CONNECTION_STRING") 
-#mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb+srv://rtvtracker:hinokamikagura@rtvgpssystem.zrtglzc.mongodb.net/?retryWrites=true&w=majority" 
+mongo = PyMongo(app)
 
 
 #Rules database information has to conform to
@@ -30,11 +32,16 @@ class TankSchema(Schema):
 
 
 @app.route("/tank", methods = ["POST"])
-def add_new_tank():
+def add_new_tank(id):
     request_dict = request.json
     try: 
-        new_tank = TankSchema().load(request_dict)
-        return "something"
+        new_gps = TankSchema().load(request_dict)
+        gps_document=mongo.db.gps.insert_one(new_gps)
+        gps_id=gps_document.inserted_id
+        gps=mongo.db.gps.find_one({"_id":gps_id})
+        gps_json=loads(dumps(gps))
+       
+        return jsonify(gps_json)
     except ValidationError as err:
         return(err.messages, 400)
 
