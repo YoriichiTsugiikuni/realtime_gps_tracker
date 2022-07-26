1341728abf9c968542e61db76a9b36a6424c09c9
Romaine Dixon <romainedixon66@gmail.com>
2022-07-25 21:59:59 -0500

    display maps

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ from json import loads
 import os
 from dotenv import load_dotenv
 from datetime import datetime
-#from flask_googlemaps import GoogleMaps
-#from flask import Flask, render_template 
-#from flask_googlemaps import Map
+from flask_googlemaps import GoogleMaps
+from flask import Flask, render_template 
+from flask_googlemaps import Map
 
 
 
@@ -40,7 +40,7 @@ class TankSchema(Schema):
 def add_new_tank():
     request_dict = request.json
     new_gps = TankSchema().load(request_dict)
-
+    
     gps_document=mongo.db.gps.insert_one(new_gps)
     gps_id=gps_document.inserted_id
 
@@ -48,6 +48,37 @@ def add_new_tank():
     gps_json=loads(dumps(gps))
        
     return jsonify(gps_json)
+
+@app.route("/")
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1419,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 37.4300,
+             'lng': -122.1400,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    return render_template('index.html', mymap=mymap, sndmap=sndmap)
+
 
 
 if __name__ == '__main__':
